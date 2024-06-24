a1038ab59d38460a80c74120349f5091e7120129
yuhao <1772156189@qq.com>
2024-06-24 09:31:41 +0800

    fix: 修复无法握手

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
 const YuFlux = require("./websocket");
 
 const yuFlux = new YuFlux("http://localhost:8000");
-yuFlux.on("upgrade", (res,) => {
-  console.error(res.headers,"res.headers");
+yuFlux.on("upgrade", (res) => {
+  console.error(res.headers, "res.headers");
 });
