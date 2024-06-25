be8eb2a3e167a13402e195d26e30be676b46134a
yuhao <76569582+yuhao423@users.noreply.github.com>
2024-06-25 15:41:24 +0800

    feat: upgrade回调函数的实现 (#17)
    * test: test
    
    * sv
    
    * fewngo
    
    * 3223
    
    * chore: 加入commit提交规范
    
    * chore: 优化commit规范
    
    * workflow: 增加ci检测及加入pr模板
    
    * fix: 修复pnpm format引起的ci问题
    
    * fix: 修复pnpm lint引起的ci问题
    
    * fix: 修复pnpm lint引起的ci问题
    
    * feat: 初始化websocket客户端
    
    * fix: 修改eslint规则,优化websocket(#6)
    
    * ci: 去除typescript的ci
    
    * refactor: 重构项目，去除typescript
    
    * feat: 加入ws 开发依赖等(#13)
    
    * fix: 修复无法握手,host=>hostName
    
    * fix: 修复无法握手
    
    * feat: upgrade回调函数的实现

--- src/constant.js
+++ src/constant.js
@@ -5,4 +5,6 @@ const readyStates = ["CONNECTING", "OPEN", "CLOSING", "CLOSED"];
 module.exports = {
   protocolVersions,
   readyStates,
+  //这个GUID只能是这个，不能是其他值
+  GUID: "258EAFA5-E914-47DA-95CA-C5AB0DC85B11",
 };
