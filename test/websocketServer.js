30af538f2e6fadf2aa6dcec1fb548af29f9435c3
yuhao <76569582+yuhao423@users.noreply.github.com>
2024-06-27 17:34:00 +0800

    test: 优化test流程 (#21)
    * test: 优化test测试

--- test/websocketServer.js
+++ test/websocketServer.js
@@ -1,7 +1,18 @@
 
 const ws = require('ws')
 
-const weServer = new ws.WebSocketServer({port:8000})
+const wsServer = new ws.WebSocketServer({port:8000})
+wsServer.on('connection',(ws)=>{
+    ws.on('error', console.error);
 
-module.exports = weServer
+    ws.on('message', function message(data) {
+      console.error('received: %s', data);
+    });
+  
+    ws.send('something');
+})
+
+module.exports = wsServer
+
+
 
