4e6a44eaf96433e1631dc72a37c5de301b4848d1
yuhao <76569582+yuhao423@users.noreply.github.com>
2024-06-27 17:54:28 +0800

    feat: 客户端实例send函数的简单实现 (#23)
    * feat: 客户端实例send函数的简单实现

--- src/constant.js
+++ src/constant.js
@@ -2,9 +2,14 @@ const protocolVersions = [13];
 
 const readyStates = ["CONNECTING", "OPEN", "CLOSING", "CLOSED"];
 
+//Buffer.alloc(size[, fill[, encoding]])： 返回一个指定大小的 Buffer 实例，如果没有设置 fill，则默认填满 0
+const emptyBuffer = Buffer.alloc(0);
+
 module.exports = {
   protocolVersions,
   readyStates,
   //这个GUID只能是这个，不能是其他值
   GUID: "258EAFA5-E914-47DA-95CA-C5AB0DC85B11",
+  //空的buffer
+  emptyBuffer,
 };
